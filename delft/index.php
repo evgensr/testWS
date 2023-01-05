0bcd830d24a7ca53e2b026ee84d56032df0a9c41
evgensr <evgensr@gmail.com>
2023-01-05 21:58:44 +0800

    first commit

---
+++ delft/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Заголовок</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	</head>
+	<body class="is-preload">
+    <?
+    phpinfo();
+    ?>
+	</body>
+</html>
